d1f5d0fc46711cbd4cc2b363d75e4d4d859b4bd5
Sukera <Seelengrab@users.noreply.github.com>
2024-01-26 18:31:53 +0100

    Initial movement experiments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+//! Renders a 2D scene containing a single, moving sprite.
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, move_sprites)
+        .run();
+}
+
+#[derive(Component)]
+enum Direction {
+    Up,
+    Down,
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("icon.png"),
+            transform: Transform::from_xyz(100., 0., 0.),
+            ..default()
+        },
+        Direction::Up,
+    ));
+}
+
+/// The sprite is animated by changing its translation depending on the time that has passed since
+/// the last frame.
+fn move_sprites(
+    time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut sprite: Query<(&mut Direction, &mut Transform)>,
+) {
+    for (mut _dir, mut transform) in sprite.iter_mut() {
+        // match *dir {
+        //     Direction::Up => transform.translation.y += ,
+        //     Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
+        // }
+        let pos_change = 1500. * time.delta_seconds();
+
+        if keyboard_input.pressed(KeyCode::Down) {
+            info!("'Down' currently pressed");
+            transform.translation.y -= pos_change;
+        }
+        if keyboard_input.pressed(KeyCode::Up) {
+            info!("'Up' currently pressed");
+            transform.translation.y += pos_change;
+        }
+        if keyboard_input.pressed(KeyCode::Left) {
+            info!("'Left' currently pressed");
+            transform.translation.x -= pos_change;
+        }
+        if keyboard_input.pressed(KeyCode::Right) {
+            info!("'Right' currently pressed");
+            transform.translation.x += pos_change;
+        }
+    }
 }
